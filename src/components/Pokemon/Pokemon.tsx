5eb114ffebe0bb602ee87bde7429b3cc7f428d76
kmurp62rulz <kmurp62rulz@gmail.com>
2022-05-06 19:13:07 +1000

    added pics

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 type Props = {
   url: string;
 };
 
 const Pokemon: React.FC<Props> = ({url}) => {
+
+  const [loading, setLoading] = useState(true);
+  const [thisPokemon, setThisPokemon] = useState("string");
+  
+  
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel: any;
+    axios
+      .get(url, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        // setNextPageUrl(res.data.next);
+        // setPrevPageUrl(res.data.previous);
+        setThisPokemon(res.data.sprites.front_default);
+        // console.log(thisPokemon);
+      });
+      
+    return () => cancel();
+  }, []);
+
   return (
-    <div>{url}</div>
+    <div>
+      <img className="w-20" src={thisPokemon} alt="/" />
+    </div>
   )
 }
 
--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import PokemonType from '../Content/Content'
-import Pokemon from '../Pokemon/Pokemon'
+import React from "react";
+import PokemonType from "../Content/Content";
+import Pokemon from "../Pokemon/Pokemon";
 
 type Props = {
   pokemon: PokemonType[];
 };
 
 const PokemonList: React.FC<Props> = ({ pokemon }) => {
-    return (
-      <div>
-        {pokemon.map((p: PokemonType) => (
-          <>
-          <div className='py-1 capitalize' key={p.url}>{p.name}</div>
-          <Pokemon url={p.url}></Pokemon>
-          </>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {pokemon.map((p: PokemonType) => (
+        <Pokemon key={p.name} url={p.url}></Pokemon>
+      ))}
+    </div>
+  );
+};
 
-export default PokemonList
+export default PokemonList;
